bdf5833a01ad3df4a1634fbd18a9f179104d1575
bastos-01 <p.bastos112@gmail.com>
2020-12-20 15:50:25 +0000

    DH encryption function test

--- client/client.py
+++ client/client.py
@@ -75,6 +75,22 @@ def main():
         except:
             break
 
+def DH_encrypt(parameters):
+
+    private_key_2 = parameters.generate_private_key()
+
+    peer_public_key_2 = parameters.generate_private_key().public_key()
+
+    shared_key_2 = private_key_2.exchange(peer_public_key_2)
+
+    derived_key_2 = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'handshake data',
+    ).derive(shared_key_2)
+
+
 if __name__ == '__main__':
     while True:
         main()
