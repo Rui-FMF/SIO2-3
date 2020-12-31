08242964a4f019df2b98594bc831adbd3adc9329
Rui-FMF <ruifmf2000@gmail.com>
2020-12-31 20:03:22 +0000

    tentativa14

--- server/server.py
+++ server/server.py
@@ -183,7 +183,7 @@ class MediaServer(resource.Resource):
         print(data)
 
         # client certificate
-        cert = x509.load_pem_x509_certificate(data['certificate'].encode('latin'))
+        cert = x509.load_pem_x509_certificate(data['certificate'])
         #print(cert.not_valid_before)
 
         # client public key
